57c1ace6d4236434d4cab4a7255189b0e1f78e73
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-03-22 21:04:39 +0100

    Add bonus question

--- switch-statements.js
+++ switch-statements.js
@@ -1,4 +1,6 @@
-// Opdracht: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
+// ==========================================
+// Opdracht 1: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
+// ==========================================
 
 const errorCode = 0;
 
@@ -13,3 +15,28 @@ const errorCode = 0;
 // const errorCode = 0 geeft "Geen watertoevoer" in de terminal
 // const errorCode = 4 geeft "Geen druk" in de terminal
 // etc.
+
+
+// ==========================================
+// Opdracht 2: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
+// ==========================================
+
+const monitorColor = "red";
+
+// Deze variabele zal altijd een kleur bevatten. De specifieke kleuren waar een melding bij gemaakt moet worden zijn: "red", "green", "yellow" en "orange".
+// Deze codes zijn gelinkt aan de volgende meldingen:
+// "green": "Hartslag stabiel"
+// "yellow": "Hartslag verhoogd"
+// "orange": "Kritieke hartslag, actie vereist!"
+// "red": "Kritieke hartslag, actie vereist!"
+// Bij alle andere kleuren is de status van de hartslag onbekend.
+
+// Verwachtte uitkomsten:
+// const monitorColor = "red" geeft "Kritieke hartslag, actie vereist!" in de terminal
+// const monitorColor = "green" geeft "Hartslag stabiel" in de terminal
+// etc.
+
+
+// ==========================================
+// [BONUSOPDRACHT] - optioneel: kun je de switch-statement uit opdracht 2 ook opschrijven met een aantal if-statements?
+// ==========================================
