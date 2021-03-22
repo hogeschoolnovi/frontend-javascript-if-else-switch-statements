329fb4dddde486f08595c262f39a50b63069e2a3
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-03-22 20:42:50 +0100

    Create exercises for conditionals

---
+++ README.md
@@ -0,0 +1,19 @@
+# Opdracht beschrijving
+
+Deze opdracht bestaat uit een aantal kleine opdrachtjes om alles wat je zojuist hebt gelezen over if-statements en switch-statements in de praktijk te brengen. Om de opdracht te maken kun je de opdracht clonen of downloaden naar jouw eigen computer.
+
+Dit project bevat de volgende bestanden:
+
+1. `conditionals.js`
+2. `switch-statements.js`
+
+Schrijf onder iedere vraag de JavaScript code die gevraagd wordt en check overal met een `console.log()` of het klopt.
+
+## Script runnen
+Als je de code wil runnen kun je dit doen door het volgende in de terminal in te voeren:
+
+`nodemon conditionals.js`
+
+Als je wisselt van bestand moet je nodemon eerst stoppen (ctrl + c voor zowel Windows als Mac) en dan opnieuw starten met de nieuwe bestandsnaam, zoals bijvoorbeeld:
+
+`nodemon switch-statements.js`
---
+++ conditionals.js
@@ -0,0 +1,38 @@
+// ==========================================
+// 0. Voorbeeldvraag: declareer een variabele met daarin de vergelijking of 2 gelijk is aan 2.                // geeft true wanneer gelogd
+// ==========================================
+
+const equalNumbers = 2 === 2;
+console.log('Is 2 gelijk aan 2?', equalNumbers);
+
+// ==========================================
+// 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
+// ==========================================
+
+
+// ==========================================
+// 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
+// ==========================================
+
+
+// ==========================================
+// 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
+// ==========================================
+
+
+// ==========================================
+// 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
+// 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
+//     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
+// ==========================================
+
+
+// ==========================================
+// 5a. Gebruik onderstaande variabelen isRaining en temperature;
+// 5b. Schrijf een script dat "Je kunt een korte broek aan!" logt wanneer het niet regent EN de temperatuur hoger is dan 21 graden
+// 5c. Log "Doe een jas aan!" Als het regent OF kouder is dan 12 graden
+// Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
+// ==========================================
+
+const isRaining = false;
+const temperature = 23;
