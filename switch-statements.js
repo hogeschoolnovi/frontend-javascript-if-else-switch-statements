c4a30f0a54db26705ae49fab1efe1782cd0f54fb
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-03-22 20:48:15 +0100

    Create switch exercise

---
+++ switch-statements.js
@@ -0,0 +1,15 @@
+// Opdracht: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
+
+const errorCode = 0;
+
+// Je mag ervanuit gaan dat deze variabele altijd een waarde tussen de 0 en de 4 bevat. Deze codes hebben de volgende betekenissen (foutmeldingen):
+// 0: "Geen watertoevoer"
+// 1: "Temperatuur te laag"
+// 2: "Koffiebonen op"
+// 3: "Afvalbak vol"
+// 4: "Geen druk"
+
+// Verwachtte uitkomsten:
+// const errorCode = 0 geeft "Geen watertoevoer" in de terminal
+// const errorCode = 4 geeft "Geen druk" in de terminal
+// etc.
